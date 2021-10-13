b65544f9176951776a02ff1d1186ba0af725bfba
markmumba <markmumba01@gmail.com>
2021-10-13 23:28:00 +0300

    Everything is working smoothly and can now display all the data from the json file

--- flaskitunes/request.py
+++ flaskitunes/request.py
@@ -3,44 +3,26 @@ from models import Music
 
 
 
-# def music() -> List:
 
-#     music_result = []
-
-#     with open('itunes.json') as f:
-#         data = json.load(f)
-
-#     for result in data['results']:
-#         if result['wrapperType'] == 'track':
-#             artistId=result['artistId']
-#             artistName=result['artistName']
-#             trackName=result['trackName']
-#             artistViewUrl=result['artistViewUrl']
-#             trackPrice=result['trackPrice']
-
-#             music_object=Music(artistId,artistName,trackName,trackPrice,artistViewUrl)
-#             music_result.append(music_object)
-
-#     return music_result
 
 def get_music(type):
-
-    
     with open ('itunes.json') as f:
         collection = json.load(f)
 
         music_list = []
         for song in collection['results']:
             if song['wrapperType'] == type:
-                artistId=song['artistId']
-                artistName=song['artistName']
-                trackName=song['trackName']
-                artistViewUrl=song['artistViewUrl']
-                trackPrice=song['trackPrice']
+                artistId=song.get('artistId')
+                artistName=song.get('artistName')
+                trackName=song.get('trackName')
+                artistViewUrl=song.get('artistViewUrl')
+                trackPrice=song.get('trackPrice')
+                artworkUrl100=song.get('artworkUrl100')
+                collectionViewUrl =song.get('collectionViewUrl')
+                collectionName=song.get('collectionName')
+                primaryGenreName=song.get('primaryGenreName')
 
-                song_object =Music(artistId,artistName,trackName,trackPrice,artistViewUrl)
-                
-
+                song_object =Music(artistId,artistName,trackName,trackPrice,artistViewUrl,artworkUrl100,collectionViewUrl,collectionName,primaryGenreName)
                 music_list.append(song_object)
     
     return music_list
